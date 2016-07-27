caaa63dcd1b2d0cfb491615e6a20e42f0ce0146d
maxotek <partho@maxotek.com>
2016-07-27 12:50:32 +0530

    Included CoursesComponent in AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello Angular</h1>'
+    template: '<h1>Hello Angular</h1><courses></courses>',
+    directives: [CoursesComponent]
 })
 export class AppComponent { }
