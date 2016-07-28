545dec732069885f42bacb758b28f0dab25a0283
maxotek <partho@maxotek.com>
2016-07-28 18:36:58 +0530

    Auto Grow Directive

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,17 +1,19 @@
 import {Component} from 'angular2/core'
 import {CourseService} from './course.service'
+import {AutoGrowDirective} from './auto-grow.directive'
 
 @Component({
     selector: "courses",
     template: `
     <h2>Courses</h2>
     {{ title }}
+    <input type="text" autoGrow style="width: 120" />
     <ul>
         <li *ngFor="#course of courses">{{ course}}</li>
     </ul>
     `,
-     providers: [CourseService]
-
+     providers: [CourseService],
+     directives: [AutoGrowDirective]
 })
 export class CoursesComponent {
     title = "The title of courses page";
